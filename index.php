a64aeee65cc96b04d919a2b3e341b5cab635c7c0
whorahulnayak <56409793+whorahulnayak@users.noreply.github.com>
2021-06-24 17:23:29 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Quiz System |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/book.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> online quiz system </h1>
+                <!-- landing page -->
+                <h3>Student Login</h3>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+                <!-- create registration for admin -->
+                <h3>Admin Login</h3>
+                <a href="admin.php" class="btn"> login </a> &emsp;
+                <a href="register1.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
---
+++ register1.php
@@ -0,0 +1,94 @@
+<?php
+	include("database.php");
+	session_start();
+	// only email pw
+	if(isset($_POST['submit']))
+	{	
+
+		$email = $_POST['email'];
+		$email = stripslashes($email);
+		$email = addslashes($email);
+
+		$password = $_POST['password'];
+		$password = stripslashes($password);
+		$password = addslashes($password);
+
+		
+		$str="SELECT email from admin WHERE email='$email'";
+		$result=mysqli_query($con,$str);
+		
+		if((mysqli_num_rows($result))>0)	
+		{
+            echo "<center><h3><script>alert('Sorry.. This email is already registered !!');</script></h3></center>";
+            header("refresh:0;url=admin.php");
+        }
+		else
+		{
+            $str="insert into admin set email='$email',password='$password'";
+			if((mysqli_query($con,$str)))	
+			echo "<center><h3><script>alert('Congrats.. You have successfully registered !!');</script></h3></center>";
+			header('location: admin.php?q=1');
+		}
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Register | Online Quiz System</title>
+		<link rel="stylesheet" href="scripts/bootstrap/bootstrap.min.css">
+		<link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
+		<link rel="stylesheet" href="css/form.css">
+        <style type="text/css">
+            body{
+                  width: 100%;
+                  background: url(image/book.png) ;
+                  background-position: center center;
+                  background-repeat: no-repeat;
+                  background-attachment: fixed;
+                  background-size: cover;
+                }
+          </style>
+	</head>
+
+	<body>
+		<section class="login first grey">
+			<div class="container">
+				<div class="box-wrapper">				
+					<div class="box box-border">
+						<div class="box-body">
+							<center> <h5 style="font-family: Noto Sans;">Register to </h5><h4 style="font-family: Noto Sans;">Online Quiz System</h4></center><br>
+							<form method="post" action="register1.php" enctype="multipart/form-data">
+                                <!-- <div class="form-group">
+									<label>Enter Your Username:</label>
+									<input type="text" name="name" class="form-control" required />
+								</div> -->
+								<div class="form-group">
+									<label>Enter Your Email Id:</label>
+									<input type="email" name="email" class="form-control" required />
+								</div>
+								<div class="form-group">
+									<label>Enter Your Password:</label>
+									<input type="password" name="password" class="form-control" required />
+                                </div>
+                                
+								<div class="form-group text-right">
+									<button class="btn btn-primary btn-block" name="submit">Register</button>
+								</div>
+								<div class="form-group text-center">
+									<span class="text-muted">Already have an account! </span> <a href="admin.php">Login </a> Here..
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<script src="js/jquery.js"></script>
+		<script src="scripts/bootstrap/bootstrap.min.js"></script>
+	</body>
+</html>
