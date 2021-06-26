e778bbceb3bd0883f47e4a5cef4136df8ed4973e
whorahulnayak <56409793+whorahulnayak@users.noreply.github.com>
2021-06-26 11:11:42 +0530

    added quiz download and score view feature

--- dashboard.php
+++ dashboard.php
@@ -69,28 +69,6 @@
 					
                 }
 
-                if(@$_GET['q']== 2) 
-                {
-                    $q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " );
-                    echo  '<div class="panel title"><div class="table-responsive">
-                    <table class="table table-striped title1" >
-                    <tr style="color:red"><td><center><b>Rank</b></center></td><td><center><b>Name</b></center></td><td><center><b>Score</b></center></td></tr>';
-                    $c=0;
-                    while($row=mysqli_fetch_array($q))
-                    {
-                        $e=$row['email'];
-                        $s=$row['score'];
-                        $q12=mysqli_query($con,"SELECT * FROM user WHERE email='$e' " )or die('Error231');
-                        while($row=mysqli_fetch_array($q12) )
-                        {
-                            $name=$row['name'];
-                            $college=$row['college'];
-                        }
-                        $c++;
-                        echo '<tr><td style="color:#99cc32"><center><b>'.$c.'</b></center></td><td><center>'.$e.'</center></td><td><center>'.$s.'</center></td>';
-                    }
-                    echo '</table></div></div>';
-                }
                 ?>
                 <?php 
                     if(@$_GET['q']==1) 
@@ -228,7 +206,7 @@
                         $email = $_SESSION['email'];
                         $result = mysqli_query($con,"SELECT * FROM quiz WHERE owned= '$email' ORDER BY date DESC") or die('Error');
                         echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
-                        <tr><td><center><b>S.N.</b></center></td><td><center><b>Topic</b></center></td><td><center><b>Total questions</b></center></td><td><center><b>Marks</b></center></td><td><center><b>Quiz ID</b></center></td><td><center><b>Action</b></center></td></tr>';
+                        <tr><td><center><b>S.N.</b></center></td><td><center><b>Topic</b></center></td><td><center><b>Total questions</b></center></td><td><center><b>Marks</b></center></td><td><center><b>Quiz ID</b></center></td><td><center><b>view scores</b></center></td><td><center><b>Action</b></center></td></tr>';
                         $c=1;
                         while($row = mysqli_fetch_array($result)) {
                             $title = $row['title'];
@@ -237,11 +215,82 @@
                             $eid = $row['eid'];
 
                             echo '<tr><td><center>'.$c++.'</center></td><td><center>'.$title.'</center></td><td><center>'.$total.'</center></td><td><center>'.$sahi*$total.'</center></td><td><center>'.$eid.'</center></td>
+                            <td><center><b><a href="dashboard.php?q=6&eid='.$eid.'" class="pull-right btn sub1" style="margin:0px;background:green;color:black"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span>&nbsp;<span class="title1"><b>View scores</b></span></a></b></center></td>
                             <td><center><b><a href="update.php?q=rmquiz&eid='.$eid.'" class="pull-right btn sub1" style="margin:0px;background:red;color:black"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Remove</b></span></a></b></center></td></tr>';
                         }
                         $c=0;
                         echo '</table></div></div>';
                     }
+                ?>
+                <?php 
+                    if(@$_GET['q']==6) 
+                    {
+                        $eid = $_GET['eid'];
+                        $result = mysqli_query($con,"SELECT * FROM history WHERE eid= '$eid' ORDER BY date DESC") or die('Error');
+                        echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
+                        <tr><td><center><b>S.N.</b></center></td><td><center><b>Email</b></center></td><td><center><b>Total Score</b></center></td>';
+                        $c=1;
+
+                        while($row = mysqli_fetch_array($result)) {
+                            $email = $row['email'];
+                            $total = $row['score'];
+                            // $sahi = $row['sahi'];
+                            // $eid = $row['eid'];
+
+                            echo '<tr><td><center>'.$c++.'</center></td><td><center>'.$email.'</center></td><td><center>'.$total.'</center></td>';  
+                      }
+                        $c=0;
+                        echo '</table>
+
+                        <form method="GET" action="export.php">
+                        <center><b><a href="dashboard.php?q=7&eid='.$eid.'" class="pull-right btn sub1" style="margin:0px;background:green;color:black"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Download</b></span></a></b></center>
+                        </form>
+
+                        </div></div>';
+                    }
+                ?>
+                <?php 
+                    if(@$_GET['q']==7) 
+                    {
+                        $eid = $_GET['eid'];
+
+                    //new modif
+                    $result = mysqli_query($con,"SELECT * FROM history WHERE eid= '$eid' ORDER BY date DESC") or die('Error');
+                    $c=1;
+                    if(mysqli_num_rows($result) > 0)
+                    {
+                     $output .= '
+                      <table class="table" bordered="1">  
+                                       <tr>  
+                                            <th>Sn</th>  
+                                            <th>Email</th>  
+                                            <th>Total</th> 
+                                       </tr>
+                     ';
+                     while($row = mysqli_fetch_array($result))
+                     {
+                      $output .= '
+                       <tr>  
+                          <td>'.$c.'</td>  
+                          <td>'.$row["email"].'</td>  
+                         <td>'.$row["total"].'</td>  
+                    
+                       </tr>
+                      ';
+                      $c++;
+                     }
+                     $c=0;
+                     $output .= '</table>';
+                     header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
+                    header("Content-Disposition: attachment; filename=abc.xls");  //File name extension was wrong
+                    header("Expires: 0");
+                    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+                    header("Cache-Control: private",false);
+                     echo $output;
+                    }
+                   }
+
+
                 ?>
             </div>
         </div>
